78183f0cc4bdec11b70443118830c966698459b2
luis angel <lusiangelgelvezdelgado@gmail.com>
2025-07-18 13:13:23 -0500

    feat: :bento: DIA 1 added

---
+++ DIA1/clase/scripts/scripts.js
@@ -0,0 +1,42 @@
+//########################
+//########Dia 1##########
+//######################
+
+//¡¡¡Primer comando¡¡¡
+console.log("Hola Mundo");
+variable1= 3.14;
+console.log(variable1);
+console.log(typeof variable1);
+//Concatenación
+palabra1="Campus"
+palabra2="Lands"
+palabra3=palabra1+palabra2
+console.log(palabra3);
+
+//Booleanos
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1);
+
+//Condicionales
+if (booleanito1 == true){
+    console.log("Es Verdadero!!");
+}
+else{
+    console.log("No es verdadero :sadfeis:");
+}
+
+//Bucle
+for(i=0;i<4;i =i+1){
+    console.log("Wenas");
+}
+
+holi = true;
+while(holi==true){
+    console.log("Wenass");
+    holi=false;
+}
+
+
+
+//Desarrollado por: Luis Gelvez - C.C:1.098.071.730
