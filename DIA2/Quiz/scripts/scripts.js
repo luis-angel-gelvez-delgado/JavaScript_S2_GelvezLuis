791a0cab017b3342e4d56dbd4b516d4d3469ad62
Luis Ángel Gelvez Delgado <luisangelgelvezdelgado1750@gmail.com>
2025-07-23 17:33:56 -0500

    feat: :bento: adding progress in menu, Monsalve helped my

--- DIA2/Quiz/scripts/scripts.js
+++ DIA2/Quiz/scripts/scripts.js
@@ -1,37 +1,84 @@
-boleanto = true
-while (Boolean)
+let ingredientes = [
+    {
+        "nombre": "Pan",
+        "descripcion": "Pan de hamburguesa clásico",
+        "precio": 1500,
+        "stock": 500
+    },
+    {
+        "nombre": "Carne de vaca",
+        "descripcion": "Carne de vaca tradicional",
+        "precio": 5000,
+        "stock": 300
+    },
+    {
+        "nombre": "Queso doble crema",
+        "descripcion": "Queso doble crema fundido",
+        "precio": 1500,
+        "stock": 200
+    }
+];
 
-console.log("=======================================");
-console.log("    Bienvenido al simulador de stock   ");
-console.log("=======================================");
-console.log("1. Crear Hamburguesa");
-console.log("2. Mostrar todas las hamburquesas");
-console.log("3. Cerrar el programa")
-let menu1 = prompt ("Elije una opcion (Numerica)"); 
-("======================================");
+let hamgur = [
+    {
+        "nombre": "Clásica",
+        "categoria": "Clásica",
+        "ingredientes": "pan","carne de vaca tradicional","Queso doble crema",
+        "precio": 8000,
+        "chef": "ChefA"
+    }
+];
 
-if (menu1 == 1);
+let chefs = [
+    {
+        "nombre": "Trevor",
+        "especialidad": "Carnes"
+    },
+    {
+        "nombre": "Franklin",
+        "especialidad": "Cocina Vegetariana"
+    },
+    {
+        "nombre": "Michael",
+        "especialidad": "Gourmet"
+    }
+];
 
-console.log("===============================");
-console.log("###### Crear hamburguesa ######");
-console.log("===============================");
-let id = prompt("escribe el ID: ");
-let NombreDeLaHamburguesa = prompt("nombre: "); 
-let descripcion = prompt("descripcion: ");
+let categorias = [
+    {
+        "nombre": "Clásica",
+        "descripcion": "Hamburguesas clásicas de toda la vida"
+    },
+    {
+        "nombre": "Vegetariana",
+        "descripcion": "Hamburguesas con carne a base de lentejas, perfectas para vegetarianos"
+    },
+    {
+        "nombre": "Gourmet",
+        "descripcion": "Hamburguesas con ingredientes la mejor calidad posible"
+    }
+];
 
-elif(menu1 == 2);
-console.log("#####################");
-console.log("#### Hamburguesa ####");
-console.log("#####################");
-console.log("ID:", listaHamburguesas[i]["id"]);
-console.log("Nombre:",listaHamburguesas[i]["nombre"]);
-console.log("Apellido:",listaHamburguesas[i]["descripcion"]),
-console.log("---------------------------");
-console.log("Hamburguesa#",q+1,":");
-console.log("#### - id:",listaHamburguesas[i]["telefonos"][q]["codigo"]);
-console.log("#### - nombre:",listaHamburguesas[i]["Hamburguesas"]);
-if(listaHamburguesas[i]["Hamburguesas"], "descripcion" == "Hamburguesas");
-console.log("#### - Descripcion: Es la descripcion de la hamburguesa");
-console.log("---------------------------");
+
+
+
+let booleanitoski = true
+while (booleanitoski == true) {
+    let opcionesMenu1 = prompt(
+    "=======================================\
+    \n    Bienvenido al simulador de stock   \
+    \n========================================\
+    \nSeleccione una opción:\
+    \n\
+    \n1. Ingresar al menu de ingredientes\
+    \n2. Ingresar al menu de Categoria\
+    \n3. Ingresar al menu de Hamburguesas\
+    \n4. Ingresar al menu de Chefs\
+    \n5. Salir\
+    ======================================");
+};
+if (menu1 == 1);{
+    
+}
 
 //Desarrollado por: Luis Angel Gelvez Delgado - C.C:1.098.071.730
