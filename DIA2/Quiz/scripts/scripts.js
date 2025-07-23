aff0159cf234d0cb403825033f5f0ea2e6f4bb0a
luis <lusiangelgelvezdelgado@gmail.com>
2025-07-23 12:32:16 -0500

    feat: :bento: I find the error, but i dont know fixed, the error is to line 32 to line 40,(I think so men, i dont know)

--- DIA2/Quiz/scripts/scripts.js
+++ DIA2/Quiz/scripts/scripts.js
@@ -2,33 +2,33 @@ let diccionarioChef1 ={
     "id":1,
     "Nombre del Chef":"Trevor",
     "Especialidad": "Carne Asiatica"
-}
+};
 let diccionarioChef2 ={
     "id":2,
     "Nombre del Chef":"Michael",
     "Especialidad": "Comida Gourmet"
-}
+};
 let diccionarioChef3 ={
     "id":3,
     "Nombre del Chef":"Franklin",
     "Especialidad": "Carne Vegetariana"
-}
+};
 
 let diccionarioHamb1 ={
     "id":1,
     "Nombre de la hamburguesa":"Clasica",
     "Descripción": "Carne sencilla"
-}
+};
 let diccionarioHamb2 ={
     "id":2,
     "Nombre de la hamburguesa":"Gourmet",
     "Descripción": "Carne Gourmet"
-}
+};
 let diccionarioHamb3 ={
     "id":3,
     "Nombre de la hamburguesa":"Clasica",
     "Descripción": "Carne Vegetariana"
-}
+};
 listaChefs=[]
 listaChefs.append(diccionarioChef1)
 listaChefs.append(diccionarioChef2)
@@ -49,7 +49,7 @@ console.log("=======================================");
 console.log("1. Crear Hamburguesa");
 console.log("2. Mostrar todas las hamburquesas");
 console.log("3. Cerrar el programa")
-let menu1 = prompt ("Elije una opcion (Numerica)") 
+let menu1 = prompt ("Elije una opcion (Numerica)"); 
 ("======================================");
 
 if (menu1 == 1);
