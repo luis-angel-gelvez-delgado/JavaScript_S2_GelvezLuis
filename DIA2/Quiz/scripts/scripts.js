24b04e24c1f954b682050f806b5d9943a0afb8cf
luis <lusiangelgelvezdelgado@gmail.com>
2025-07-23 12:23:36 -0500

    feat: :bento: add errors in ¡page for Gooooooooodgit statusgit status i dont know master, but i can

--- DIA2/Quiz/scripts/scripts.js
+++ DIA2/Quiz/scripts/scripts.js
@@ -29,7 +29,6 @@ let diccionarioHamb3 ={
     "Nombre de la hamburguesa":"Clasica",
     "Descripción": "Carne Vegetariana"
 }
-
 listaChefs=[]
 listaChefs.append(diccionarioChef1)
 listaChefs.append(diccionarioChef2)
@@ -49,23 +48,31 @@ console.log("    Bienvenido al simulador de stock   ");
 console.log("=======================================");
 console.log("1. Crear Hamburguesa");
 console.log("2. Mostrar todas las hamburquesas");
-console.log("3. Mostrar los ingredientes de una hamburguesa individual")
-console.log("4. Actualizar los ingredientes de una hamburguesa")
-console.log("5. Borrar una hamburguesa de la faz de la tienda")
-console.log("6. Cerrar el programa")
+console.log("3. Cerrar el programa")
 let menu1 = prompt ("Elije una opcion (Numerica)") 
 ("======================================");
 
 if (menu1 == 1);
 
-console.log("===============================")
-console.log("###### Crear hamburguesa ######")
-console.log("===============================")
-diccionarioHamburguesas={
-    "i":input ("escribe el ID: "),
-    "nombre de la hamburguesa ":input("nombre: "), 
-    "descripcion": input("descripcion: "),
-} 
+console.log("===============================");
+console.log("###### Crear hamburguesa ######");
+console.log("===============================");
+let id = prompt("escribe el ID: ");
+let NombreDeLaHamburguesa = prompt("nombre: "); 
+let descripcion = prompt("descripcion: ");
 
-
+elif(menu1 == 2);
+console.log("#####################");
+console.log("#### Hamburguesa ####");
+console.log("#####################");
+console.log("ID:", listaHamburguesas[i]["id"]);
+console.log("Nombre:",listaHamburguesas[i]["nombre"]);
+console.log("Apellido:",listaHamburguesas[i]["descripcion"]),
+console.log("---------------------------");
+console.log("Hamburguesa#",q+1,":");
+console.log("#### - id:",listaHamburguesas[i]["telefonos"][q]["codigo"]);
+console.log("#### - nombre:",listaHamburguesas[i]["Hamburguesas"]);
+if(listaHamburguesas[i]["Hamburguesas"], "descripcion" == "Hamburguesas");
+console.log("#### - Descripcion: Es la descripcion de la hamburguesa");
+console.log("---------------------------");
 //Desarrollado por: Luis Angel Gelvez Delgado - C.C:1.098.071.730
