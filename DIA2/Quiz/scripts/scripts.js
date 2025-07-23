dd5f1d9c7e9f6229964c1f01a7e1e1bff9c5ee88
luis <lusiangelgelvezdelgado@gmail.com>
2025-07-23 12:01:55 -0500

    feat: :bento: add the start to page of hamburguersMenu

---
+++ DIA2/Quiz/scripts/scripts.js
@@ -0,0 +1,71 @@
+let diccionarioChef1 ={
+    "id":1,
+    "Nombre del Chef":"Trevor",
+    "Especialidad": "Carne Asiatica"
+}
+let diccionarioChef2 ={
+    "id":2,
+    "Nombre del Chef":"Michael",
+    "Especialidad": "Comida Gourmet"
+}
+let diccionarioChef3 ={
+    "id":3,
+    "Nombre del Chef":"Franklin",
+    "Especialidad": "Carne Vegetariana"
+}
+
+let diccionarioHamb1 ={
+    "id":1,
+    "Nombre de la hamburguesa":"Clasica",
+    "Descripción": "Carne sencilla"
+}
+let diccionarioHamb2 ={
+    "id":2,
+    "Nombre de la hamburguesa":"Gourmet",
+    "Descripción": "Carne Gourmet"
+}
+let diccionarioHamb3 ={
+    "id":3,
+    "Nombre de la hamburguesa":"Clasica",
+    "Descripción": "Carne Vegetariana"
+}
+
+listaChefs=[]
+listaChefs.append(diccionarioChef1)
+listaChefs.append(diccionarioChef2)
+listaChefs.append(diccionarioChef3)
+
+listaHamburguesas=[]
+listaHamburguesas.append(diciionarioHamburguesa1)
+listaHamburguesas.append(diciionarioHamburguesa2)
+listaHamburguesas.append(diciionarioHamburguesa3)
+
+
+boleanto = true
+while (Boolean)
+
+console.log("=======================================");
+console.log("    Bienvenido al simulador de stock   ");
+console.log("=======================================");
+console.log("1. Crear Hamburguesa");
+console.log("2. Mostrar todas las hamburquesas");
+console.log("3. Mostrar los ingredientes de una hamburguesa individual")
+console.log("4. Actualizar los ingredientes de una hamburguesa")
+console.log("5. Borrar una hamburguesa de la faz de la tienda")
+console.log("6. Cerrar el programa")
+let menu1 = prompt ("Elije una opcion (Numerica)") 
+("======================================");
+
+if (menu1 == 1);
+
+console.log("===============================")
+console.log("###### Crear hamburguesa ######")
+console.log("===============================")
+diccionarioHamburguesas={
+    "i":input ("escribe el ID: "),
+    "nombre de la hamburguesa ":input("nombre: "), 
+    "descripcion": input("descripcion: "),
+} 
+
+
+//Desarrollado por: Luis Angel Gelvez Delgado - C.C:1.098.071.730
