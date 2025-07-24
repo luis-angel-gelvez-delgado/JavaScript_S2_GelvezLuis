46357e3c864563ce0ae04a1f7c38ccfb06c41094
Luis Ángel Gelvez Delgado <luisangelgelvezdelgado1750@gmail.com>
2025-07-23 19:48:43 -0500

    feat: :bento: order in code yujuuuu(yes, Monsalve helpedd me)

--- DIA2/Quiz/scripts/scripts.js
+++ DIA2/Quiz/scripts/scripts.js
@@ -23,7 +23,7 @@ let hamgur = [
     {
         "nombre": "Clásica",
         "categoria": "Clásica",
-        "ingredientes": "pan, carne de vaca tradicional, Queso doble crema", 
+        "ingredientes": "pan, carne de vaca tradicional, Queso doble crema",
         "precio": 8000,
         "chef": "ChefA"
     }
@@ -65,7 +65,7 @@ let categorias = [
 let booleanitoski = true
 while (booleanitoski == true) {
     let menu1 = prompt(
-    "=======================================\
+        "=======================================\
     \n    Bienvenido al simulador de Hamburguesas   \
     \n========================================\
     \nSeleccione una opción:\
@@ -75,52 +75,48 @@ while (booleanitoski == true) {
     \n3. Ingresar al menu de Hamburguesas\
     \n4. Ingresar al menu de Chefs\
     \n5. Salir\
-    ======================================")
-;
-
-if (menu1 == "1") {
+    ======================================");
+    if (menu1 == "1") {
         let menu2 = prompt(
-        "===========================================\
-        \n            Menu de Ingredientes          \
-        \n===========================================\
-        \nSeleccione una opción:\
-        \n\
-        \n1. Añadir nuevo Ingrediente\
-        \n2. Eliminar Ingrediente\
-        \n3. Actualizar Ingrediente\
-        \n4. Ver ingredientes\
-        \n5. Salir\
-        \n==========================================="
+            "===========================================\
+            \n            Menu de Ingredientes          \
+            \n===========================================\
+            \nSeleccione una opción:\
+            \n\
+            \n1. Añadir nuevo Ingrediente\
+            \n2. Eliminar Ingrediente\
+            \n3. Actualizar Ingrediente\
+            \n4. Ver ingredientes\
+            \n5. Salir\
+            \n==========================================="
         )
-
-
-
-if (menu2 == "1") {
-    let nombreIng = prompt("Ingresa el nombre del ingrediente que deseas añadir");
-    let dscpIng = prompt("Ingresa la descripción del ingrediente");
-    let valorIng = prompt("Ingresa el valor del ingrediente");
-    let stock = prompt("Ingresa la cantidad dispoible del ingrediente");
-    let seguro = prompt("Segur@ que deseas añadir este ingrediente? \n1. Si \n2. No\n");
-    if (seguro == "1"){
-        ingredientes.push({
-            "nombreIng": nombreIng,
-            "dscpIng": dscpIng,
-            "valorIng": valorIng,
-            "stock": stock,
-        });
-        alert("El ingrediente fue agregado correctamente")
-    }else if (seguro == "2"){
-        alert("Opción no agregada")
-    } else {
-        alert("Opción incorrecta, volviendo al menu Principal")
+        if (menu2 == "1") {
+            let nombreIng = prompt("Ingresa el nombre del ingrediente que deseas añadir");
+            let dscpIng = prompt("Ingresa la descripción del ingrediente");
+            let valorIng = prompt("Ingresa el valor del ingrediente");
+            let stock = prompt("Ingresa la cantidad dispoible del ingrediente");
+            let seguro = prompt("Segur@ que deseas añadir este ingrediente? \n1. Si \n2. No\n");
+            if (seguro == "1") {
+                ingredientes.push({
+                    "nombre": nombreIng,
+                    "descripcion": dscpIng,
+                    "precio": valorIng,
+                    "stock": stock,
+                });
+                alert("El ingrediente fue agregado correctamente")
+            } else if (seguro == "2") {
+                alert("Opción no agregada")
+            } else {
+                alert("Opción incorrecta, volviendo al menu Principal")
+            }
         }
-    }} 
-    else if (menu1 == 2){
+    }
+    else if (menu1 == 2) {
         let eliminar = prompt("Ingrese el ingrediente a eliminar");
         ingredientes.splice(eliminar - 1);
         alert("Ingrediente eliminado")
     };
-    
-        }
+
+}
 
 //Desarrollado por: Luis Angel Gelvez Delgado - C.C:1.098.071.730
