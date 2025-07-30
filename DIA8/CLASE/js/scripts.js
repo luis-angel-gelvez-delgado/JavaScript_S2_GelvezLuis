003e93b191c01a752c125c00a7df67ba909d2090
luis angel <lusiangelgelvezdelgado@gmail.com>
2025-07-30 07:29:01 -0500

    feat: :bento: i understandgit add .git add . characters added (i think so)

--- DIA8/CLASE/js/scripts.js
+++ DIA8/CLASE/js/scripts.js
@@ -2,10 +2,10 @@
  // Mediante XMLHttpRequest
 
 //Modularizar el consumo de la api
-function buscarPersonaje(){
+function buscarAlgo(){
     const nombreUsar = prompt("Ingresar el nombre del personaje a buscar");
     const xhr = new XMLHttpRequest(); //La importación del XML
-    const url = `https://rickandmortyapi.com/api/character?name=${nombreUsar}`; //Acento Inverso ```
+    const url = `https://swapi.dev/api/people?=${nombreUsar}`; //Acento Inverso ```
     console.log(url);
     xhr.open("GET",url, true);
     xhr.onreadystatechange = function () {
@@ -17,7 +17,7 @@ function buscarPersonaje(){
             try{
 
                 const daticos = JSON.parse(xhr.responseText);
-                alert(`The character is: Name: "${daticos["results"][0]["name"]}" Status: "${daticos["results"][0]["status"]}" Species: "${daticos["results"][0]["species"]}"`);
+                alert(`The character is: Name: "${daticos["results"][0]["name"]}" Height: "${daticos["results"][0]["height"]}" Mass: "${daticos["results"][0]["mass"]}" Hair_color: "${daticos["results"][0]["hair_color"]}" Skin_color: "${daticos["results"][0]["skin_color"]}" Eye_color: "${daticos["results"][0]["eye_color"]}"   Birth_year: "${daticos["results"][0]["birth_year"]}" Gender: "${daticos["results"][0]["gender"]}" Homeworld: "${daticos["results"][0]["Homeworld"]}" films: "${daticos["results"][0]["films"]}" Species: "${daticos["results"][0]["species"]}" vehicles: "${daticos["results"][0]["vehicles"]}" Starship: "${daticos["results"][0]["starship"]}" Created: "${daticos["results"][0]["created"]}" Edited: "${daticos["results"][0]["edited"]}" Url: "${daticos["results"][0]["url"]}" `);
             }catch(err){
                 console.log(err.message); //Si algo malo pasa simplemente imprima el error
             }
@@ -25,4 +25,4 @@ function buscarPersonaje(){
     };
     xhr.send();
 }
-buscarPersonaje();
+buscarAlgo();
