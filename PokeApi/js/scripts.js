df2de0cd7009b1ed09b073c08b5d48a00e24b217
Mvteiio <romancamargo02@gmail.com>
2025-07-31 13:08:11 -0500

    feat: :bento: changes in btns, complet style and fix errors

--- PokeApi/js/scripts.js
+++ PokeApi/js/scripts.js
@@ -1,29 +1,30 @@
- // Consumo de API
- // Mediante XMLHttpRequest
 
-//Modularizar el consumo de la api
-/*
 function buscarAlgo(){
-    const nombreUsar = prompt("Ingresar el nombre del personaje a buscar");
+    document.getElementById("results").innerHTML=``;
+    const nombreUsar = document.getElementById("buscar").value.trim();
+    console.log(nombreUsar);
     const xhr = new XMLHttpRequest(); //La importación del XML
-    const url = `https://swapi.dev/api/people?=${nombreUsar}`; //Acento Inverso ```
-    console.log(url);
+    const url = `https://pokeapi.co/api/v2/pokemon/${nombreUsar}`; //Acento Inverso ```+
     xhr.open("GET",url, true);
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 3){
-            alert("Cargando...")
-        }
-
-        else if (xhr.readyState === 4 && xhr.status === 200){
+     
+        if (xhr.readyState === 4 && xhr.status === 200){
             try{
-
-                const daticos = JSON.parse(xhr.responseText);
-                alert(`The character is: Name: "${daticos["results"][0]["name"]}" Height: "${daticos["results"][0]["height"]}" Mass: "${daticos["results"][0]["mass"]}" Hair_color: "${daticos["results"][0]["hair_color"]}" Skin_color: "${daticos["results"][0]["skin_color"]}" Eye_color: "${daticos["results"][0]["eye_color"]}"   Birth_year: "${daticos["results"][0]["birth_year"]}" Gender: "${daticos["results"][0]["gender"]}" Homeworld: "${daticos["results"][0]["Homeworld"]}" films: "${daticos["results"][0]["films"]}" Species: "${daticos["results"][0]["species"]}" vehicles: "${daticos["results"][0]["vehicles"]}" Starship: "${daticos["results"][0]["starship"]}" Created: "${daticos["results"][0]["created"]}" Edited: "${daticos["results"][0]["edited"]}" Url: "${daticos["results"][0]["url"]}" `);
+                let data = JSON.parse(xhr.responseText);
+                let results = document.getElementById("results");
+                results.innerHTML += `
+                <p class="num"${data["id"]}</p>
+                <p class="nPoke">${data["name"]}</p>
+                `
+                
+                
             }catch(err){
-                console.log(err.message); //Si algo malo pasa simplemente imprima el error
+                console.log(err.message); 
             }
         }
+        
     };
     xhr.send();
 }
-buscarAlgo();*/
+
+buscarAlgo()
