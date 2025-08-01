458e643029aad2851d0a7b106dd1d9ba67374451
luis angel <lusiangelgelvezdelgado@gmail.com>
2025-08-01 07:28:08 -0500

    feat: :hammer: added more content

--- PokeApi/js/scripts.js
+++ PokeApi/js/scripts.js
@@ -1,30 +1,37 @@
 
-function buscarAlgo(){
-    document.getElementById("results").innerHTML=``;
+function buscarPoke() {
+    document.getElementById("results").innerHTML = ``;
     const nombreUsar = document.getElementById("buscar").value.trim();
     console.log(nombreUsar);
     const xhr = new XMLHttpRequest(); //La importación del XML
     const url = `https://pokeapi.co/api/v2/pokemon/${nombreUsar}`; //Acento Inverso ```+
-    xhr.open("GET",url, true);
+    console.log(url);
+    xhr.open("GET", url, true);
+
     xhr.onreadystatechange = function () {
-     
-        if (xhr.readyState === 4 && xhr.status === 200){
-            try{
-                let data = JSON.parse(xhr.responseText);
-                let results = document.getElementById("results");
-                results.innerHTML += `
-                <p class="num"${data["id"]}</p>
-                <p class="nPoke">${data["name"]}</p>
-                `
+        if (xhr.readyState === 3) {
+
+            console.log("Cargando...")
+        }
+
+        else if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                //if (data.results && data.results.length > 0) {
+                //    for (let i = 0; i < data.results.length; i++) {
+                let division = document.querySelector(".resultado");
                 
-                
-            }catch(err){
-                console.log(err.message); 
+                console.log(data["name"]);
+                //}
+                //}
+            } catch (err) {
+                console.log(err.message);
             }
         }
-        
+
     };
     xhr.send();
-}
+};
 
-buscarAlgo()
+buscarPoke();
