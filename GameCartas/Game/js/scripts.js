620de70c8f2f0d0d231f793a78cc36d35762c0c6
Luis Ángel Gelvez Delgado <luisangelgelvezdelgado1750@gmail.com>
2025-08-04 19:07:27 -0500

    feat: :hammer: added functions for buttoms

--- GameCartas/Game/js/scripts.js
+++ GameCartas/Game/js/scripts.js
@@ -1,6 +1,7 @@
-function revelarCartas(cards) {
+async function revelarCartas(cards) {//aqui esta la funcion revelarCartas
     document.getElementById('susCards').innerHTML = `<img src= ${cards[0]['image']} alt="noCarga">`
-    document.getElementById('misCards').innerHTML = `<img src="https://www.deckofcardsapi.com/static/img/back.png" alt="noCarga">`
+    document.getElementById('misCards').innerHTML = `<img src=${cards[1]['image']} alt="noCarga">`
+    console.log("hello")
 }
 function repartirCartas(idMazo) {
 
@@ -11,15 +12,18 @@ function repartirCartas(idMazo) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             try {
-
+                const btnMenor = document.getElementById('btnMenor')
+                const btnMayor = document.getElementById('btnMayor')
+                const btnEqual = document.getElementById('btnEqual')
                 const misCards = document.getElementsByClassName("misCards")
-
                 const data = JSON.parse(xhr.responseText);
                 const cards = data['cards']
                 console.log(cards)
                 document.getElementById('susCards').innerHTML = `<img src="https://www.deckofcardsapi.com/static/img/back.png" alt="noCarga">`
                 document.getElementById('misCards').innerHTML = `<img src="https://www.deckofcardsapi.com/static/img/back.png" alt="noCarga">`
-                revelarCartas(cards)
+                btnMenor.addEventListener('click', ()=>{ revelarCartas(cards)})
+                btnMayor.addEventListener('click', ()=>{ revelarCartas(cards)})
+                btnEqual.addEventListener('click', ()=>{ revelarCartas(cards)})
             }
             catch (err) {
                 alert(err.message);
