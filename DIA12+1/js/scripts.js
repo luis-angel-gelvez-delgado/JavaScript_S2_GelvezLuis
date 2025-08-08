9584add5e0fea0b9ecbb3653c01dd5f3b58b3256
Luis Angel <lusiangelgelvezdelgado@gmail.com>
2025-08-08 08:05:03 -0500

    feat: :hammer: added Doc in DIA12+1  javaScript

--- DIA12+1/js/scripts.js
+++ DIA12+1/js/scripts.js
@@ -3,22 +3,24 @@ function buscarSuperHero() { //esta es la funcion ¿Principal?
     const nombreUsar = document.getElementById("nombreInput").value.trim();//const(Nombre de la variable) nombreUsar(el nombre de la variable)//
     console.log(nombreUsar)
     const xhr = new XMLHttpRequest();
-    const url = `https://www.superheroapi.com/api.php/4b999b744359c464f472528e61e32049/search/${nombreUsar}`;
-    console.log(url);
-    xhr.open("GET", url, true);
+    const url = `https://www.superheroapi.com/api.php/4b999b744359c464f472528e61e32049/search/${nombreUsar}`;//Aqui va el link, y en la partee de {nombreUsar}, tal y como indica , colocaras el nombre a buscar, por ejemplo: hulk
+    console.log(url);//Muestra un link en consola que contiene toda la data del(os) personaje(s) buscado(s), y es de donde sacaremos la data completa (nombre, edad, id, color de pelo, de ojos, status, trabajo, etc.. )y es de donde mas trade sacaramos la data requerida
+    xhr.open("GET", url, true);//entra en el conseguido, para empezar a conseguir los datos
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 3) {
-            console.log("cargando")
-        }
-        else if (xhr.readyState === 4 && xhr.status === 200) {
-            try {
-                const data = JSON.parse(xhr.responseText);
-                console.log(data)
-                if (data.results && data.results.length > 0) {
-                    for (let i = 0; i < data.results.length; i++) {
+        if (xhr.readyState === 3) {// si es igual a 3, significa que esta cargando, no es necesario, pero es buena practica creo
+            console.log("cargando")// Muestra un mensaje de "cargando"en cago de que el readyState sea 3
+        } 
+        else if (xhr.readyState === 4 && xhr.status === 200) {// el readyState 4 significa que la operacion se completo, y el status 200 significa que la solicitud ha tenido exito
+            try {// Hacemos un try donde estara el bloque de codigo que tal vez pueda tener un error, para de esa manera buscarle solución
+                const data = JSON.parse(xhr.responseText); // El jason.parse sirve para convertir opcionalmente una cadena de texto, en una estructura JSON (se ve mas ordenado)
+                console.log(data)// Muestra toda la data obtenida del(os) personaje(s) buscado(s), y es de donde vamos a extraer lo requerido, como el nombre, id, imagen, etc...
+                if (data.results && data.results.length > 0) {// EL operador && (AND) sirve para? NPI de momento, pero creo que es para obtener el largo de la data extraida
+                    for (let i = 0; i < data.results.length; i++) {// // En las siguientes lineas estaremos extrayendo la data requerida ya mencionada
                         let division = document.getElementById("resultados");
-                        division.innerHTML += `
-                        <div class="card">
+                        division.innerHTML += /*NPI de por que la division*/
+                        // El div a continuacion es para acomodar de manera ordenada la data que estaremos extrayendo, y a continuación: La data
+                        //
+                        `<div class="card">
                             <img src="${data["results"][i]["image"]["url"]}">
                             <h3>${data["results"][i]["name"]}</h3>
                             <p><strong>ID:</strong>${data["results"][i]["id"]}
@@ -34,42 +36,18 @@ function buscarSuperHero() { //esta es la funcion ¿Principal?
     }
     xhr.send();
 }
+/* Los readyState
+0	UNSENT	Client has been created. open() not called yet.
+1	OPENED	open() has been called.
+2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+3	LOADING	Downloading; responseText holds partial data.
+4	DONE	The operation is complete.
+ */
 
-//Vamos con los document
-
-/* 
- 1.  Document.activeElement //                               Devuelve el Element activo actualmente
- 2.  Document.body //                                        Devuelve el nodo <body> o <frameset> del documento actual
- 3.  Document.characterSet //                                Devuelve el conjunto de caracteres que utiliza el documento
- 4.  Document.childElementCount //                           Devuelve el número de caracteres quee utiliza el documento
- 5.  Document.children //                                    Devuelve los elementos hjos del documento actual
- 6.  Document.compatMode //                                  Indica si el documento se presenta en modo //quirks// o strict
- 7.  Document.contentType //                                 Devuelve el tipo contenido del encabezado MIME del documento actual
- 8.  Document.currentScript //                               Devuelve el Elemento <script> cuyo script se esta procesando actualmente y //no es un modulo de JavaScript//
- 9.  Document.doctype //                                     Devuelve la Definición de Tipo de Documento (DTD) del documento actual
- 10. Document. //                                            Devuelve el Element activo actualmente
- 11. Document.documentElementRead only //                    Devuelve el Element que es un hijo directo del documento. Para documentos HTML, normalmente es el objeto HTMLHtmlElement que representa el elemento <html> del documento
- 12. Document.documentURIRead only //                        Devuelve la ubicación del documento como una cadena.
- 13. Document.embedsRead only //                             Devuelve un HTMLCollection de los elementos <embed> incrustados en el documento.
- 14. Document.firstElementChild Read only //                 Devuelve el primer elemento hijo del documento actual.
- 15. Document.fonts //                                       Devuelve la interfaz FontFaceSet del documento actual.
- 16. Document.formsRead only //                              Devuelve un HTMLCollection de los elementos <form> del documento.
- 17. Document.fullscreenElement Read only //                 El elemento que está actualmente en modo de pantalla completa para este documento.
- 18. Document.headRead only //                               Devuelve el elemento <head> del documento actual.
- 19. Document.hiddenRead only //                             Devuelve un valor booleano que indica si la página se considera oculta o no.
- 20. Document.imagesRead only //                             Devuelve un HTMLCollection de las imágenes del documento.
- 21. Document.implementationRead only //                     Devuelve la implementación DOM asociada con el documento actual.
- 22. Document.lastElementChild Read only //                  Devuelve el último elemento hijo del documento actual.
- 23. Document.linksRead only //                              Devuelve un HTMLCollection de los hipervínculos del documento.
- 24. Document.mozSyntheticDocument No estándar //            Devuelve true solo si este documento es sintético, como una imagen independiente, video, archivo de audio o similar.
- 25. Document.pictureInPictureElement Read only //           Devuelve el Element que se presenta actualmente en modo picture-in-picture en este documento.
- 26. Document.pictureInPictureEnabled Read only //           Devuelve true si la función picture-in-picture está habilitada.
- 27. Document.pluginsRead only //                            Devuelve un HTMLCollection de los complementos disponibles.
- 28. Document.pointerLockElement Read only //                Devuelve el conjunto de elementos como destino para los eventos del mouse mientras el puntero está bloqueado. null si el bloqueo está pendiente, el puntero está desbloqueado o si el objetivo está en otro documento.
- 29. Document.featurePolicy ExperimentalRead only //         Devuelve la interfaz FeaturePolicy que proporciona una API simple para analizar las políticas de funciones aplicadas a un documento específico.
- 30. Document.scriptsRead only //                            Devuelve un HTMLCollection de los elementos <script> del documento.
- 31. Document.scrollingElementRead only //                   Devuelve una referencia al Element que desplaza el documento.
- 32. Document.styleSheets Read only //                       Devuelve un objeto StyleSheetList de CSSStyleSheet para hojas de estilo explícitamente vinculadas o incrustadas en un documento.
- 33. Document.timeline ExperimentalRead only //              Devuelve la línea de tiempo como una instancia especial de DocumentTimeline que se crea automáticamente al cargar la página.
- 34. Document.visibilityStateRead only //                    Devuelve un string que indica el estado de visibilidad del documento. Los valores posibles son visible, hidden, prerender y unloaded.
-*/
+/* El Status
+Respuestas informativas (100–199),
+Respuestas satisfactorias (200–299),
+Redirecciones (300–399),
+Errores de los clientes (400–499),
+y errores de los servidores (500–599).
+ */
